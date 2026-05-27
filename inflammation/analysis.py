c0c9cd40b914d744a7c02bdf9d802b281f5b35a5
trixie555 <p.kappler@tudelft.nl>
2026-05-27 11:23:59 +0200

    added csvdatasource

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -8,13 +8,19 @@ import argparse
 
 from inflammation import models, views
 
-def load_inflammation_data(data_dir):
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
 
-    return list(data)
+class CSVDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+        data = map(models.load_csv, data_file_paths)
+
+        return list(data)
 
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
@@ -23,7 +29,8 @@ def analyse_data(data_dir):
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
 
-    data = load_inflammation_data(data_dir)
+    data_source = CSVDataSource(data_dir)
+    data = data_source.load_inflammation_data()
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
