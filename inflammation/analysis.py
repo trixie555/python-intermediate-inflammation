556de923e2b6b08e4957e139a00183d171c86681
trixie555 <p.kappler@tudelft.nl>
2026-05-27 10:00:58 +0200

    added test analysis

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -26,10 +26,8 @@ def analyse_data(data_dir):
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
 
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+    print(daily_standard_deviation)
+    return daily_standard_deviation
 
 if __name__ == '__main__':
     # Initialize the parser
